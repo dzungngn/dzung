e497a73b60adcd94f2d0a83cd0c202b6679fc84c
dzvng.ngn <dzvng.ngn@gmail.com>
2022-02-15 23:01:36 +0700

    Update

--- group project/src/components/Main.js
+++ group project/src/components/Main.js
@@ -8,6 +8,7 @@ let arr=["Popular","Theater", "Kids", "Drama", "Comedy"]
 const Main=()=>{
     const [movieData,setData]=useState([]);
     const [url_set,setUrl]=useState(url);
+    const [search,setSearch]=useState();
 
     useEffect(()=>{
         fetch(url_set).then(res=>res.json()).then(data=>{
@@ -15,6 +16,38 @@ const Main=()=>{
             setData(data.results);
         });
     },[url_set])
+    const getData=(movieType)=>{
+        if(movieType=="Popular")
+        {
+            url=base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
+        }
+        if(movieType=="Theater")
+        {
+            url=base_url+"/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22"+API_key;
+        }
+        if(movieType=="Kids")
+        {
+            url=base_url+"/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc"+API_key;
+        }
+        if(movieType=="Drama")
+        {
+            url=base_url+"/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10"+API_key;
+        }
+        if(movieType=="Comedy")
+        {
+            url=base_url+"/discover/movie?with_genres=35&with_cast=23659&sort_by=revenue.desc"+API_key;
+        }
+        setUrl(url);
+        
+    }
+    const searchMovie=(evt)=>{
+        if(evt.key=="Enter")
+        {
+            url=base_url+"/search/movie?api_key=ee3dc9b1faa44ec833e51492b2974e7d&query="+search;
+            setUrl(url);
+            setSearch(" ");
+        }
+    }
     return(
         <>
             <div className="header">
@@ -23,7 +56,7 @@ const Main=()=>{
                         {
                             arr.map((value)=>{
                                 return(
-                                    <li><a href="#" name={value}>{value}</a></li>
+                                    <li><a href="#" name={value} onClick={(e)=>{getData(e.target.name)}}>{value}</a></li>
                                 )
                             })
                         }
@@ -31,9 +64,10 @@ const Main=()=>{
                 </nav>
                 <form>
                     <div className="search-btn">
-                        <input type="text" placeholder="Enter Movie Name" className="inputText">
+                        <input type="text" placeholder="Enter Movie Name" className="inputText" onChange={(e)=>{setSearch(e.target.value)}} 
+                        value={search} onKeyPress={searchMovie}>
                         </input>
-                        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                        <button><i class="fas fa-search"></i></button>
                     </div>
                 </form>
             </div>
